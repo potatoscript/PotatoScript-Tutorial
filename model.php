a5d23959bafa4f27cf48354dea7ca37d1a12f8e6
BRUCE LIM <potatoscript@yahoo.com>
2024-02-12 19:24:45 +0900

    Update model.php

--- model.php
+++ model.php
@@ -1,15 +1,11 @@
 <?php
 header('Content-Type:application/json');
 $datalist=array();
-$data=array();
 
-$data[0]="名前";
-$data[1]="個数";
-array_push($datalist,$data);
-
-$data[0]="potato";
-$data[1]="1";
-array_push($datalist,$data);
+$datalist={
+{0:"名前",1:"個数"},
+{0:"potato",1:"5"},
+}
 
 echo json_encode($datalist);
 
